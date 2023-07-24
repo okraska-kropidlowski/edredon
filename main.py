553457df5a707fbf43d6ccb16596d7960f563322
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-07-24 20:34:44 +0200

    Work further on main menu

--- main.py
+++ main.py
@@ -17,14 +17,26 @@ def view_entries():
 def add_entry():
     os.system('add_entry.py')
 
+def about():
+    os.system('about.py')
+
 #Window and widgets layout
 frame = tkinter.Frame(main_window)
 frame.pack()
 
-view_entries_button = tkinter.Button(frame, text="View entries", command=view_entries)
-view_entries_button.grid(row=1, column=0, sticky="news", padx=20, pady=10)
+menu_label = tkinter.LabelFrame(frame, text="MAIN MENU")
+menu_label.grid(row=1, column=0)
 
-add_entry_button = tkinter.Button(frame, text="Add entry", command=add_entry)
-add_entry_button.grid(row=1, column=1, sticky="news", padx=20, pady=10)
+view_entries_button = tkinter.Button(menu_label, text="View entries", command=view_entries)
+view_entries_button.grid(row=0, column=0, sticky="news", padx=20, pady=10)
+
+add_entry_button = tkinter.Button(menu_label, text="Add entry", command=add_entry)
+add_entry_button.grid(row=1, column=0, sticky="news", padx=20, pady=10)
+
+profiles_button = tkinter.Button(menu_label, text="Profiles", command=view_entries)
+profiles_button.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+
+about_button = tkinter.Button(menu_label, text="About", command=about)
+about_button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
 
 main_window.mainloop()

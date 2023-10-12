4789124086135c2ad75e93db70e6213b46ccb0eb
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-10-13 00:37:15 +0200

    Add profile removal

--- profiles.py
+++ profiles.py
@@ -26,6 +26,20 @@ def add_profile():
             profiles_file.write(new_profile)
             add_profile_box.delete('1.0', 'end')
 
+def profile_selection(profile):
+    global to_be_deleted
+    profile = remove_profile_list.get()
+    to_be_deleted = profile
+    print(to_be_deleted)
+
+def remove_profile():
+    with open('data\profiles_list', "r") as profiles_file:
+        profiles_file_content = profiles_file.read()
+
+    with open('data\profiles_list', "w") as profiles_file:
+        removed_profile = profiles_file_content.replace(to_be_deleted, "")
+        profiles_file.write(removed_profile)
+
 #Window and widgets layout
 profiles_label = tkinter.LabelFrame(profiles, text="MANAGE PROFILES")
 profiles_label.grid(column=0)
@@ -38,9 +52,10 @@ add_button.grid(column=1, row=0)
 
 remove_profile_list = ttk.Combobox(profiles_label, state="readonly", values=tuple(profiles_list))
 remove_profile_list.current=""
+remove_profile_list.bind("<<ComboboxSelected>>", profile_selection)
 remove_profile_list.grid(column=0, row=1)
 
-remove_button = tkinter.Button(profiles_label, text="Remove profile")
+remove_button = tkinter.Button(profiles_label, text="Remove profile", command=remove_profile)
 remove_button.grid(column=1, row=1)
 
 for widget in profiles_label.winfo_children():

70ce6f7488e0c81819a86c1b4729e5f478189f00
okraska-kropidlowski <wojtek.1986@gmail.com>
2023-12-26 22:04:18 +0100

    Add "about" window

--- about.py
+++ about.py
@@ -8,4 +8,8 @@ about.title("About edredon")
 about.iconbitmap("data/images/edredon.ico")
 about.resizable(False, False)
 
+info = tkinter.Label(about, text="edredon version 1.0\n\nFirst release with basic functionality:\n- Profile management\n- Entry adding/viewing\n\nCreated using:\n- TkinterMapView by Tom Schimansky\n- sqlite3 by Gerhard Häring\n\nList of bird species taken from:\nhttp://komisjafaunistyczna.pl/lista/\n")
+info.config(justify=tkinter.LEFT, padx=20, pady=10, font=("Arial", 10))
+info.pack()
+
 about.mainloop()
